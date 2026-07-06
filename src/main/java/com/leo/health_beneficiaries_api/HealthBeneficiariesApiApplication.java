9f9b2bd6a8ad8d07a93368a60d8b570dc329d19b
LeoAugustoDev <leoaugustoalves58@gmail.com>
2026-07-06 13:54:55 -0300

    first commit

---
+++ src/main/java/com/leo/health_beneficiaries_api/HealthBeneficiariesApiApplication.java
@@ -0,0 +1,13 @@
+package com.leo.health_beneficiaries_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBeneficiariesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBeneficiariesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/health_beneficiaries_api/HealthBeneficiariesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.health_beneficiaries_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBeneficiariesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
